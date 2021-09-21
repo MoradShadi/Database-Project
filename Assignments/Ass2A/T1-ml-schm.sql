654e61db40a54a149a8ae5fe160e5ed8f3a55002
mabo0003 <mabo0003@MVNDCSTUL03008.ad.monash.edu>
2021-09-21 20:35:55 +0800

    create table entries for task 1

--- Assignments/Ass2A/T1-ml-schm.sql
+++ Assignments/Ass2A/T1-ml-schm.sql
@@ -1,9 +1,9 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --T1-ml-schm.sql
 
---Student ID:
---Student Name:
---Tutorial No:
+--Student ID: 29799260
+--Student Name: Morad Abou Shadi
+--Tutorial No: FIT3171 tut 2
 
 /* Comments for your marker:
 
@@ -17,13 +17,33 @@
 -- are included. FK constraints are to be added at the end of this script
 
 -- BOOK_COPY
+CREATE TABLE book_copy (
+    branch_code         NUMBER(2) NOT NULL,
+    bc_id      NUMBER(6) NOT NULL,
+    bc_purchase_price      NUMBER(7,2) NOT NULL,
+    bc_counter_reserve CHAR(1) NOT NULL,
+    book_call_no VARCHAR2(20) NOT NULL
+);
 
 
 -- LOAN
-
+CREATE TABLE loan (
+    branch_code         NUMBER(2) NOT NULL,
+    bc_id      NUMBER(6) NOT NULL,
+    loan_date_time      DATE NOT NULL,
+    loan_due_date   DATE NOT NULL,
+    loan_actual_return_date DATE,
+    bor_no NUMBER(6) NOT NULL
+);
 
 -- RESERVE
-
+CREATE TABLE reserve (
+    reserve_id          NUMBER(6) NOT NULL,
+    branch_code         NUMBER(2) NOT NULL,
+    bc_id      NUMBER(6) NOT NULL,
+    reserve_date_time_placed      DATE NOT NULL,
+    bor_no NUMBER(6) NOT NULL
+);
 
 -- Add all missing FK Constraints below here
 
