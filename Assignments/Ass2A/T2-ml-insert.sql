384ab092dc1ce601c751f5ab990da1d117416798
mabo0003 <mabo0003@MVNDCSTUL03005.ad.monash.edu>
2021-09-26 23:50:08 +0800

    insert statments for book copy table

--- Assignments/Ass2A/T2-ml-insert.sql
+++ Assignments/Ass2A/T2-ml-insert.sql
@@ -1,9 +1,9 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --T2-ml-insert.sql
 
---Student ID:
---Student Name:
---Tutorial No:
+--Student ID: 29799260
+--Student Name: Morad Abou Shadi
+--Tutorial No: FIT3171 tut 2
 
 /* Comments for your marker:
 
@@ -14,3 +14,98 @@
 
 -- 2 (a) Load the BOOK_COPY, LOAN and RESERVE tables with your own
 -- test data following the data requirements expressed in the brief
+INSERT INTO book_copy VALUES (
+    10,
+    1,
+    100.20,
+    'N',
+    '005.74 D691D'
+);
+
+INSERT INTO book_copy VALUES (
+    10,
+    2,
+    100.20,
+    'N',
+    '005.74 D691D'
+);
+
+INSERT INTO book_copy VALUES (
+    10,
+    3,
+    100.20,
+    'Y',
+    '005.74 D691D'
+);
+
+INSERT INTO book_copy VALUES (
+    10,
+    4,
+    99.99,
+    'N',
+    '005.432 L761P'
+);
+
+INSERT INTO book_copy VALUES (
+    11,
+    1,
+    89.99,
+    'N',
+    '112.6 S874D'
+);
+
+INSERT INTO book_copy VALUES (
+    11,
+    2,
+    99.99,
+    'N',
+    '005.432 L761P'
+);
+
+INSERT INTO book_copy VALUES (
+    11,
+    3,
+    99.99,
+    'Y',
+    '005.432 L761P'
+);
+
+INSERT INTO book_copy VALUES (
+    13,
+    1,
+    100.20,
+    'N',
+    '005.74 D691D'
+);
+
+INSERT INTO book_copy VALUES (
+    13,
+    2,
+    149.99,
+    'N',
+    '823.914 H219A'
+);
+
+INSERT INTO book_copy VALUES (
+    13,
+    3,
+    129.99,
+    'N',
+    '823.914 A211H'
+);
+
+INSERT INTO book_copy VALUES (
+    13,
+    4,
+    129.99,
+    'Y',
+    '823.914 A211H'
+);
+
+INSERT INTO book_copy VALUES (
+    13,
+    5,
+    89.99,
+    'N',
+    '112.6 S874D'
+);
