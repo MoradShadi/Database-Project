94974a6a0f8f32163fa8390ef09acd8750bddfd9
mabo0003 <mabo0003@MVNDCSTUL03005.ad.monash.edu>
2021-09-27 01:29:06 +0800

    insert statements for loans

--- Assignments/Ass2A/T2-ml-insert.sql
+++ Assignments/Ass2A/T2-ml-insert.sql
@@ -109,3 +109,93 @@ INSERT INTO book_copy VALUES (
     'N',
     '112.6 S874D'
 );
+
+INSERT INTO loan VALUES (
+    10,
+    1,
+    TO_DATE('2021-06-02 10:34:09', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-06-16', 'YYYY-MM-DD'),
+    TO_DATE('2021-06-16', 'YYYY-MM-DD'),
+    1
+);
+
+INSERT INTO loan VALUES (
+    10,
+    1,
+    TO_DATE('2021-06-16 18:39:19', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-06-30', 'YYYY-MM-DD'),
+    TO_DATE('2021-06-22', 'YYYY-MM-DD'),
+    1
+);
+
+INSERT INTO loan VALUES (
+    10,
+    4,
+    TO_DATE('2021-06-11 18:39:19', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-06-25', 'YYYY-MM-DD'),
+    TO_DATE('2021-06-24', 'YYYY-MM-DD'),
+    1
+);
+
+INSERT INTO loan VALUES (
+    11,
+    1,
+    TO_DATE('2021-06-03 19:49:09', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-06-17', 'YYYY-MM-DD'),
+    TO_DATE('2021-06-18', 'YYYY-MM-DD'),
+    2
+);
+
+INSERT INTO loan VALUES (
+    11,
+    2,
+    TO_DATE('2021-06-03 19:49:09', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-06-17', 'YYYY-MM-DD'),
+    TO_DATE('2021-06-18', 'YYYY-MM-DD'),
+    2
+);
+
+INSERT INTO loan VALUES (
+    11,
+    1,
+    TO_DATE('2021-07-03 10:37:10', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-07-17', 'YYYY-MM-DD'),
+    TO_DATE('2021-07-25', 'YYYY-MM-DD'),
+    2
+);
+
+INSERT INTO loan VALUES (
+    11,
+    2,
+    TO_DATE('2021-08-15 05:28:06', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-08-29', 'YYYY-MM-DD'),
+    NULL,
+    3
+);
+
+INSERT INTO loan VALUES (
+    13,
+    2,
+    TO_DATE('2021-07-01 15:48:06', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-07-15', 'YYYY-MM-DD'),
+    TO_DATE('2021-07-10', 'YYYY-MM-DD'),
+    5
+);
+
+INSERT INTO loan VALUES (
+    13,
+    5,
+    TO_DATE('2021-07-16 15:48:06', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-07-30', 'YYYY-MM-DD'),
+    TO_DATE('2021-07-29', 'YYYY-MM-DD'),
+    5
+);
+
+INSERT INTO loan VALUES (
+    13,
+    3,
+    TO_DATE('2021-08-16 23:42:16', 'YYYY-MM-DD HH24:MI:SS'),
+    TO_DATE('2021-08-30', 'YYYY-MM-DD'),
+    NULL,
+    5
+);
