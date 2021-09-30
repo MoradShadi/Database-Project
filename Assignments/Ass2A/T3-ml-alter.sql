4b9556d93a09543f5a00aecde85c6bac83874a73
mabo0003 <mabo0003@MVNDCSTUL03008.ad.monash.edu>
2021-09-30 18:43:44 +0800

    Task 3a

--- Assignments/Ass2A/T3-ml-alter.sql
+++ Assignments/Ass2A/T3-ml-alter.sql
@@ -1,9 +1,9 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --T3-ml-alter.sql
 
---Student ID:
---Student Name:
---Tutorial No:
+--Student ID: 29799260
+--Student Name: Morad Abou Shadi
+--Tutorial No: FIT3171 tut 2
 
 /* Comments for your marker:
 
@@ -14,12 +14,32 @@
 
 -- 3 (a)
 
+ALTER TABLE book_copy
+ADD book_cond char(1) default 'G' Not NULL;
 
+COMMENT ON COLUMN book_copy.book_cond IS
+    'A book is damaged (D) or lost (L). If the book is not damaged or lost, then it
+is good (G) ';
 
+ALTER TABLE book_copy
+    ADD CONSTRAINT bc_cond_chk CHECK ( book_cond IN ( 'D', 'L', 'G' ) );
+    
+commit;
 
+update book_copy
+set book_cond = 'L'
+where branch_code = (
+        SELECT
+            branch_code
+        FROM
+            branch
+        WHERE
+            branch_contact_no = 0395601655
+    ) AND book_call_no = '005.74 C824C';
+
+commit;
 
 -- 3 (b)
-
 
 
 
